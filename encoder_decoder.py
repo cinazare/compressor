bec8a4e85af661310c40a985232b4e58a969ed2e
cina <its.cina.zare@gmail.com>
2024-03-25 17:57:37 +0430

    adding 'writer' function to write the data in txt file in byte mode

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -4,6 +4,87 @@ def encoder(adress):
     tree_node_dict = get_data_from_txt(adress)
     hufman_code = huffmancode_executer(tree_node_dict)
 
+    # making an strong with my huffman_code string
+    hufman_code = hufman_code.split(",")
+    del hufman_code[0]
+
+
+    hufman_code_dict = dict()
+    
+    for temp in hufman_code:
+        temp = temp.split(":")
+        hufman_code_dict[int(temp[0].strip())] = temp[1].strip()
+
+    lenth = 0
+    for key, value in hufman_code_dict.items():
+        lenth +=  tree_node_dict[str(key)] * len(value)
+    
+    writer(hufman_code_dict, adress, lenth)
+
+    return hufman_code_dict
+
+def writer(huffman_code, adress, lenth):
+    commpressed_addres = adress
+    for temp in range(1,len(commpressed_addres)):
+        
+        if commpressed_addres[-1] == "/":
+            commpressed_addres = commpressed_addres[:-1]
+            break
+        
+        else:
+            commpressed_addres = commpressed_addres[:-1]
+     
+
+    dictionary = open(commpressed_addres +'/compressed.bin', "w", encoding="utf-8")
+
+    # writing the dictionary
+    for key, value in huffman_code.items(): 
+        if chr(key) == "1" or chr(key) == "0":
+            dictionary.write("'" + chr(key) + "'" + value)
+        else: 
+            dictionary.write(chr(key)+value)
+
+    dictionary.write("$" + str(lenth) + "$")
+    dictionary.write("\n")
+
+    dictionary.close()
+    
+    txt = open(adress, "r")
+    compress = open(commpressed_addres +'/compressed.bin', "ab")
+    
+    txt_lines = txt.read()
+    
+    string = str()
+    for letter in txt_lines:
+        string += huffman_code[int(ord(letter))]
+    
+
+    while True:
+        if len(string) % 8 != 0:
+            string += "0"
+        else:
+            break
+    bytes = _to_Bytes(string)
+    compress.write(bytes)        
+        
+    print("your file is compressed")
+    txt.close()
+    compress.close()        
+
+
+
+
+
+def _to_Bytes(data):
+    b = list()
+    for i in range(0, len(data), 8):
+        b.append(int(data[i:i+8], 2))
+    b = bytearray(b)
+    return b      
+
+
+
+
 
 
 
@@ -91,3 +172,5 @@ def huffman_tree_maker(huffman_list):
             if huffman_list[index].sequence >= new_node.sequence:
                 huffman_list.insert(index, new_node)
                 break
+
+
--- tree_maker.py
+++ tree_maker.py
@@ -1,2 +1,39 @@
 class TREE:
-    pass
+    def __init__(self, ascii, sequence = 0, right = None, left = None, dad = None):
+        self.ascii = ascii
+        self.sequence = sequence
+        self.right = right
+        self.left = left
+        self.dad = dad
+
+    def append_to_right(self, other):
+        if self.right != None:
+            print("the right node is full already")
+            return
+    
+        self.right = other
+        other.dad = self
+        return
+    def append_to_left(self, other):
+        
+        if self.left != None:
+            print("the right node is full already")
+            return
+    
+        self.left = other
+        other.dad = self
+        return
+    
+
+    def display(self):
+        if  self.left:
+            self.left.display()
+            self.right.display()
+            print(self.ascii, " : ", self.sequence)
+            return
+        elif self.right:
+            self.right.display()
+            print(self.ascii, " : ", self.sequence)
+            return
+        else:
+            print(self.ascii, " : ", self.sequence)
